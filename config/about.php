d4dc1d873759f3a9c7547954ce5d1e428815ec8d
shubhamkanchar <shubhamkanchar687@gmail.com>
2022-09-05 18:42:39 +0530

    address changes

--- config/about.php
+++ config/about.php
@@ -4,7 +4,7 @@ return [
     'company_name'=>'GreatEWay',
     'email'=>'contact@greateway.com',
     'mobile1'=>'9874563210',
-    'mobile2'=>'9874563210',
-    'address'=>'pune',
+    'mobile2'=>'8380952885',
+    'address'=>'Office 101, 01st Floor, Stellar Spaces, Kharadi South Main Road, opp. Zensar, IT Park, Kharadi, Pune, Maharashtra 411014',
     'full_name'=>'GreatEWay Software pvt. Ltd.'
 ];
